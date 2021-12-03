3e78a99fd9c2781675742202a9d7fe40e531a8ba
SolbiShin <sbshin.dev@gmail.com>
2021-12-03 18:20:32 +0900

    redux-thunk를 이용하여 비동기 작업 처리

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import rootReducer from './modules';
 import { createLogger } from 'redux-logger';
+import ReduxThunk from 'redux-thunk';
+
 
 const logger = createLogger();
-const store = createStore(rootReducer, applyMiddleware(logger));
+const store = createStore(rootReducer, applyMiddleware(logger, ReduxThunk));
 
 ReactDOM.render(
   <Provider store={store}>
---
+++ src/modules/sample.js
@@ -0,0 +1,35 @@
+import { handleActions } from "redux-actions";
+import * as api from '../lib/api';
+import createRequestThunk from "../lib/createRequestThunk";
+
+const GET_POST = 'sample/GET_POST';
+const GET_POST_SUCCESS = 'sample/GET_POST_SUCCESS';
+const GET_POST_FAILURE = 'sample/GET_POST_FAILURE';
+
+const GET_USERS = 'sample/GET_USERS';
+const GET_USERS_SUCCESS = 'sample/GET_USERS_SUCCESS';
+const GET_USERS_FAILURE = 'sample/GET_USERS_FAILURE';
+
+export const getPost = createRequestThunk(GET_POST, api.getPost);
+export const getUsers = createRequestThunk(GET_POST, api.getUsers);
+
+const initialState = {
+    post: null,
+    users: null
+};
+
+const sample = handleActions(
+    {
+        [GET_POST_SUCCESS]: (state, action) => ({
+            ...state,
+            post: action.payload
+        }),
+        [GET_USERS_SUCCESS]: (state, action) => ({
+            ...state,
+            users: action.payload
+        })
+    },
+    initialState
+);
+
+export default sample;
